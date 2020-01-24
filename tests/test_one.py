3f4a96e9ca62183460839fbbcdd95ccbb41aa5b6
Colton Garelli <coltongarelli@gmail.com>
2020-01-24 16:32:51 -0500

    main in test file.......

--- tests/test_one.py
+++ tests/test_one.py
@@ -5,3 +5,11 @@ def test_a():
     assert 'y' == 'y'
     assert 'x' == 'y'
     assert 'x' == 'y'
+
+def test_b():
+    print('i totally forgot to include a main maybe thats why this wont run')
+
+
+if __name__ == '__main__':
+    test_a()
+    test_b()
