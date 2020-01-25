d76474977855f986ef8440134c14c561a68cff1a
Colton Garelli <coltongarelli@gmail.com>
2020-01-25 16:24:31 -0500

    fix environment.yml

--- tests/test_one.py
+++ tests/test_one.py
@@ -4,13 +4,14 @@ import pytest
 def test_a():
     assert 'x' == 'x'
     assert 'y' == 'y'
-    assert 'x' == 'y'
+
     assert 'x' == 'y'
 
 
 def test_b():
-    print('i totally forgot to include a main maybe thats why this wont run')
-
+    assert 'x' == 'y', "We didnt do it, boys"
+    with pytest.raises(AssertionError):
+        assert 'x' == 'y'
 
 if __name__ == '__main__':
     test_a()
