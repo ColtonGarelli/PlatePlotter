cbfb6dca7d9ee24a35e318f4d28af63a2ce6320a
Colton Garelli <coltongarelli@gmail.com>
2020-01-25 16:27:22 -0500

    fix environment.yml

--- tests/test_one.py
+++ tests/test_one.py
@@ -5,13 +5,13 @@ def test_a():
     assert 'x' == 'x'
     assert 'y' == 'y'
 
-    assert 'x' == 'y'
+
 
 
 def test_b():
-    assert 'x' == 'y', "We didnt do it, boys"
+
     with pytest.raises(AssertionError):
-        assert 'x' == 'y'
+        assert 'x' == 'y', "We didnt do it, boys"
 
 if __name__ == '__main__':
     test_a()
